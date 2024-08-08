95ddffc1f52e3394f5c22208dd64af5ae25fcf29
G Bisaccia <argovarius@gmail.com>
2024-08-08 17:55:05 +0200

    cleaning in data-fetch.R

--- scripts/data-fetch.R
+++ scripts/data-fetch.R
@@ -3,19 +3,12 @@ library(dplyr)
 library(bib2df)
 library(bibliometrix)
 
-# card / rad journals derived from scimagojr
-#journals_card <- read_xlsx("data/raw/journals-cardiology.xlsx")
-#journals_rad <- read_xlsx("data/raw/journals-radiology.xlsx")
-
 # top 10% journals derived from scopus
 journals_all <- read_xlsx("data/scopus.xlsx")
 
-# to add testthat implementation to check whether binding leads to data loss?
-# library(testthat)
-
 # load papers from pubmed
 #source("R/pubmed-data-fetch.R")
-#papers <- readRDS("data/pubmed-export_2024-08-08.RDS")
+papers <- readRDS("data/pubmed-export_2024-08-08.RDS")
 
 papers_n <- papers$Articles
 
